21a38ea307e1f97303ce0c63b9256f3b4b2cb4b0
JStrahan7813 <102590661+JStrahan7813@users.noreply.github.com>
2026-06-15 16:56:23 +0100

    Update cypress.config.js
    change to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,17 @@
+const { defineConfig } = require("cypress");
 const fs = require('fs');
-// 1. Import the Allure plugin reporter
-const { allureCypress } = require("@allure-javascript/cypress/reporter");
+// Update this line to use the correct package name
+const { allureCypress } = require("allure-cypress/reporter"); 
 
-module.exports = {
+module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // 2. Initialize the Allure plugin and specify the output directory
       allureCypress(on, config, {
         resultsDir: "allure-results",
       });
 
-      // 3. Your existing credential saving task remains untouched
       on('task', {
         saveCredentials(credentials) {
           fs.writeFileSync('cypress/test-credentials.txt', credentials);
@@ -20,8 +19,7 @@ module.exports = {
         }
       });
 
-      // 4. Crucial: You must return the config object so Allure settings are registered by Cypress
       return config;
     },
   },
-};
+});
